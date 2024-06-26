ab74afa8239c05359a0d27123e8e1489aed98c5e
Camille <lecamille@gmail.com>
2024-06-26 13:22:25 -0700

    Fix this

--- myproject/slackbot/apps.py
+++ myproject/slackbot/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SlackbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myproject.slackbot'
+    name = 'slackbot'
